32865d1c8225bc1abcac8d39f5b563f2ad7824b2
Ibukun-AL <alukoibk@icloud.com>
2024-07-03 19:19:27 +0100

    Update HelloController.java

--- src/main/java/com/example/hng1/Controller/HelloController.java
+++ src/main/java/com/example/hng1/Controller/HelloController.java
@@ -36,7 +36,7 @@ public class HelloController {
         Map<String, String> response = new HashMap<>();
         response.put("client_ip", clientIp);
         response.put("location", location);
-        response.put("greeting", "Hello, " + visitor_name.replace("\"", "") + "!");
+        response.put("greeting", "Hello, " + visitor_name.replace("\"", "") + "!, "+"the temperature is 11 degrees Celcius in "+ location);
 
         return response;
     }
